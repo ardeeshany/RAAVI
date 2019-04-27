6a1f8b1e64d2a9d37316bd9eb0e8242b49b89bcc
ardeeshany <ardeeshany@gmail.com>
2019-04-27 11:12:19 -0700

    # box report fix in server

--- Module/MainModules/M0_Box.R
+++ Module/MainModules/M0_Box.R
@@ -275,18 +275,19 @@ M0_Box <- function(input,output,session,Vals,format_out,font_plot){
     
 
     filename = function(){
-    paste('گزارش کلاس', sep = '.', switch(format_out(),HTML = 'html', PDF = 'pdf', Word = 'docx'))
+    paste('report', sep = '.', switch(format_out(),HTML = 'html', PDF = 'pdf', Word = 'docx'))
     },
     content=function(file){
       withProgress(message = "... گزارش در حال ساخته شدن است",
                    min = 0,max = 100,value = 72, {
       tempReport <- file.path(tempdir(),"box.Rmd")
-      file.copy("report/box.Rmd",tempReport,overwrite = TRUE)
+      file.copy("~/report/box.Rmd",tempReport,overwrite = TRUE)
       params <- list(n = Reac_CP2M_Bx()$gg1,m=Reac_CP2M_Bx()$gg2)
       rmarkdown::render(tempReport,output_format = switch(format_out(),PDF = pdf_document(), HTML = html_document(), Word = word_document()),
                         output_file = file,
                         params = params,
-                        envir = new.env(parent = globalenv()))})})
+                        envir = new.env(parent = globalenv()))
+      })})
   
   
 }
