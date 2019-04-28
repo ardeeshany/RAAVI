457de8f8ec8581e290f8a3d0e8022c6d8025a0fe
ardeeshany <ardeeshany@gmail.com>
2019-04-27 18:46:16 -0700

    # fix bugs

--- Module/MainModules/M0_Box.R
+++ Module/MainModules/M0_Box.R
@@ -25,7 +25,7 @@ div(style="text-align:center;",
                     
                     div(style="text-align:center",
                     actionBttn(inputId = ns("Bx_Ac"),style = "jelly",color = "warning",
-                               label= div(class="action-button--widget","هفتم"))),
+                               label= div(class="action-button--widget","هشتم"))),
                     
                     br(),
                     uiOutput(ns("output"))
